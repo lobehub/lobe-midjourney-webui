8cc70f12bc84092f6e887ea6d9bd8473e64f9c94
arvinxx <arvinx@foxmail.com>
2024-01-20 17:59:41 +0800

    :bug: fix: fix discord url

--- src/app/home/Header.tsx
+++ src/app/home/Header.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link';
 import { memo } from 'react';
 import { Flexbox } from 'react-layout-kit';
 
-import { GITHUB_REPO } from '@/const/url';
+import { DISCORD_URL, GITHUB_REPO } from '@/const/url';
 import Settings from '@/features/Settings';
 
 const Header = memo(() => (
   <Flexbox align={'center'} horizontal justify={'space-between'}>
     <Logo extra={'Midjourney WebUI'} type={'combine'} />
     <Flexbox horizontal>
-      <Link href={GITHUB_REPO} style={{ color: 'inherit' }} target={'_blank'}>
+      <Link href={DISCORD_URL} style={{ color: 'inherit' }} target={'_blank'}>
         <ActionIcon icon={DiscordIcon} title={'Discord'} />
       </Link>
       <Link href={GITHUB_REPO} style={{ color: 'inherit' }} target={'_blank'} title={'Github Repo'}>
