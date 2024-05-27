17fbe0060a4757c369a2ffd4cf93188751c4dc66
arvinxx <arvinx@foxmail.com>
2024-05-28 00:44:35 +0800

    :bug: fix: upgrade deps and fix build

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    optimizePackageImports: ['@icons-pack/react-simple-icons', '@lobehub/ui'],
+    optimizePackageImports: ['@icons-pack/react-simple-icons', '@lobehub/ui', 'shiki'],
     webVitalsAttribution: ['CLS', 'LCP'],
   },
   images: {
